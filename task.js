a5c5db1de1af35239060ab9dfb791a83f8d695b1
Alina Prish <alinaprish92@mail.ru>
2023-06-28 17:07:25 +0300

    add revision

--- task.js
+++ task.js
@@ -1,21 +1,21 @@
 function getArrayParams(...arr) {
-  let min = arr[0];
-  let max = arr[0];
-  let sum = 0;
-  for (let i of arr) {
+  //let min = arr[0];
+  //let max = arr[0];
+ /*for (let i of arr) {
     if (max < i) {
       max = i;
-    }
-    if (min > i) {
+    }if (min > i) 
       min = i;
-    }
-  for (let i = 0; i < arr.length; i++){
-    sum += arr[i]
-  }
-  return sum;
+  //for (let i = 0; i < arr.length; i++){
+  }*/
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
 
-  let avg = sum / arr.length;
-}
+  let sum = arr.reduce(function(currentSum, currentNumber) {
+    return currentSum + currentNumber;
+  }, 0)
+  let avg1 = sum / arr.length.toFixed(2);
+  let avg = Number.parseFloat(avg1)
 }
 return { min: min, max: max, avg: avg };
 
@@ -31,6 +31,7 @@ function summElementsWorker(...arr) {
 function differenceMaxMinWorker(...arr) {
   let min = arr[0];
   let max = arr[0];
+  let rezult = max - min;
   for (let i of arr) {
     if (max < i) {
       max = i;
@@ -39,15 +40,16 @@ function differenceMaxMinWorker(...arr) {
       min = i;
     }
   }
-  return {max, min};
+  return {rezult};
 }
 
 function differenceEvenOddWorker(...arr) {
   let sumEvenElement = 0;
   let sumOddElement = 0;
 
-  for (let i = 0; i < arr.length; i++){
-    if (i / 2 == 0){
+  //for (let i = 0; i < arr.length; i++){
+  for (let element of arr){
+    if (element / 2 == 0){
       sumEvenElement++
     }
     else{
@@ -63,23 +65,25 @@ function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0;
   let countEvenElement = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (i / 2 == 0) {
-      sumEvenElement += i;
+  //for (let i = 0; i < arr.length; i++) {
+    for (let element of arr){
+    if (element / 2 == 0) {
+      sumEvenElement += element;
       countEvenElement++
     }
     return countEvenElement / sumEvenElement
   }
 }
 
-function makeWork (arrOfArr,...func) {
+function makeWork (arrOfArr, func) {
   let maxWorkenResult = Infinity;
+  let a = 0;
   for (let i = 0; i < arrOfArr.length; i++){
-    function func(...i) {
-      return i;
-    }
-    if (i > maxWorkenResult) {
-      maxWorkenResult += i;
+    func(...i)
+      return a == i;
+    
+    if (a > maxWorkenResult) {
+      maxWorkenResult += a;
     }
 }
 
